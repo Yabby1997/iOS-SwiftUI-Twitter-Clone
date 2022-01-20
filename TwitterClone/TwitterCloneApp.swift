630e519c9fc49909dcca35cd8a87abc30c9f29c5
yabby <yabby1997@gmail.com>
2022-01-20 20:08:50 +0900

    initial commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Seunghun Yang on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
